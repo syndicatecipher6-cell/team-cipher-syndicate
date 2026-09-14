5cd726a604ef3c2fe8e9a69f136495c358d4f67f
hreempp007 <hreempradhan321@gmail.com>
2026-09-14 22:10:47 +0530

    fix: build errors due to missing env variables

--- src/lib/neo4j.ts
+++ src/lib/neo4j.ts
@@ -1,8 +1,8 @@
 import neo4j from 'neo4j-driver';
 
-const uri = process.env.NEO4J_URI!;
-const user = process.env.NEO4J_USERNAME!;
-const password = process.env.NEO4J_PASSWORD!;
+const uri = process.env.NEO4J_URI || 'neo4j://localhost'
+const user = process.env.NEO4J_USERNAME || 'neo4j'
+const password = process.env.NEO4J_PASSWORD || 'password'
 
 // Initialize a Neo4j Driver instance
 export const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
